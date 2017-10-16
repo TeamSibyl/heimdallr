665d7879a2b4b39c1ff05ef4bea6aecb51dc299a
Sumeet Padavala <sumeet.padavala@gmail.com>
2017-10-16 10:37:41 +0530

    Super basic profiler implementation

--- heimdallr/profilerengine.py
+++ heimdallr/profilerengine.py
@@ -1,11 +1,11 @@
 from angr import Project
 from profile import Profile
-
+from itertools import chain
 
 class ProfilerEngine(object):
     """
     A basic class that, given details about a segment, produces a profile
-    for it. It's main interface is the profile(segment) method
+    for it. This class is the most complex part of this project
     """
     def __init__(self, angr_args):
         self.b = Project(angr_args[0], **angr_args[1])
@@ -15,5 +15,12 @@ class ProfilerEngine(object):
         The main interface to this class. Given a segment desciption, produce a
         profile descibing its functioning. Pure function.
         """
-        # TODO implement this to produce a profile
-        return Profile("blargh", {"a", "1234"})
+	a = [block.vex.operations for block in segment.blocks]
+	a = list(chain(*a))
+	d = {}	
+	for op in a:
+		if op in d:
+			d[i]+=1
+		else:
+			d[i] = 1
+	return Profile(segment.name, d)
